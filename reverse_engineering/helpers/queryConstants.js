1da7f8a7893d92237b063409dd3006c71660afc3
Vitalii Yarmus <vitalii.yarmys@binary-studio.com>
2021-09-24 11:57:29 +0300

    RE: removed partition data from query

--- reverse_engineering/helpers/queryConstants.js
+++ reverse_engineering/helpers/queryConstants.js
@@ -3,11 +3,9 @@ module.exports = {
     GET_SCHEMA_NAMES: 'SELECT schema_name FROM information_schema.schemata;',
     GET_TABLE_NAMES: 'SELECT * FROM information_schema.tables WHERE table_schema = $1 ORDER BY table_name;',
     GET_NAMESPACE_OID: 'SELECT oid FROM pg_namespace WHERE nspname = $1',
-    GET_TABLE_LEVEL_DATA: `SELECT pc.relpersistence, pc.reloptions, pt.spcname, ppt.partstrat, ppt.partattrs
+    GET_TABLE_LEVEL_DATA: `SELECT pc.relpersistence, pc.reloptions, pt.spcname
         FROM pg_class AS pc 
         LEFT JOIN pg_tablespace AS pt 
         ON pc.reltablespace = pt.oid
-        LEFT JOIN pg_partitioned_table AS ppt
-		ON pc.oid = ppt.partrelid
         WHERE pc.relname = $1 AND pc.relnamespace = $2;`,
 };
