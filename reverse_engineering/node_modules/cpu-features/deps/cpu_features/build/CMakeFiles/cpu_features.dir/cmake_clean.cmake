0291716536332c64ad8d2c6105fc2bac81e1a9cb
Vitalii Yarmus <vitalii.yarmys@binary-studio.com>
2021-10-29 15:51:37 +0300

    RE: updated library for ssh tunneling

---
+++ reverse_engineering/node_modules/cpu-features/deps/cpu_features/build/CMakeFiles/cpu_features.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpu_features.dir/src/cpuinfo_x86.c.o"
+  "libcpu_features.pdb"
+  "libcpu_features.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cpu_features.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
