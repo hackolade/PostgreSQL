744c259f91cb8e2479dfb7b407c1811d42953a25
Vitalii Yarmus <vitalii.yarmys@binary-studio.com>
2021-10-29 13:47:25 +0300

    Added log about system information

--- reverse_engineering/helpers/loggerHelper.js
+++ reverse_engineering/helpers/loggerHelper.js
@@ -1,3 +1,7 @@
+const os = require('os');
+const net = require('net');
+const packageFile = require('../../package.json');
+
 const createLogger = ({ title, logger, hiddenKeys }) => {
     return {
         info(message, additionalData = {}) {
@@ -20,6 +24,49 @@ const prepareError = error => {
     return error;
 };
 
+const getPluginVersion = () => packageFile.version;
+
+const getSystemInfo = appVersion => {
+    return (
+        '' +
+        `Date: ${new Date()}` +
+        '\n' +
+        `Application version: ${appVersion}` +
+        '\n' +
+        `Plugin version: ${getPluginVersion()}` +
+        '\n\n' +
+        `System information:` +
+        '\n' +
+        ` Hostname:  ${os.hostname()}` +
+        '\n' +
+        ` Platform:  ${os.platform()} ${os.arch()}` +
+        '\n' +
+        ` Release:   ${os.release()}` +
+        '\n' +
+        ` Uptime:    ${toTime(os.uptime())}` +
+        '\n' +
+        ` Total RAM: ${(os.totalmem() / 1073741824).toFixed(2)} GB` +
+        '\n' +
+        ` CPU Model: ${os.cpus()[0].model}` +
+        '\n' +
+        ` CPU Clock: ${maxClock(os.cpus())} MHZ` +
+        '\n' +
+        ` CPU Cores: ${os.cpus().length} cores` +
+        '\n\n'
+    );
+};
+
+const maxClock = cpus => {
+    return cpus.reduce((highestClock, cpu) => Math.max(highestClock, cpu.speed), 0);
+};
+
+const prefixZero = number => (number < 10 ? '0' + number : number);
+
+const toTime = number => {
+    return Math.floor(number / 3600) + ':' + prefixZero(parseInt((number / 3600 - Math.floor(number / 3600)) * 60));
+};
+
 module.exports = {
     createLogger,
+    getSystemInfo,
 };
