6e2d3353beecf8916854cadc1737cf8bf5b38eb4
Vitalii Yarmus <vitalii.yarmys@binary-studio.com>
2023-03-29 17:36:22 +0300

    RE: implement reverse-engineering of primary keys with single key as pk on field level

--- reverse_engineering/helpers/postgresService.js
+++ reverse_engineering/helpers/postgresService.js
@@ -45,6 +45,7 @@ const {
 } = require('./postgresHelpers/viewHelper');
 const { setDependencies: setDependenciesInTriggerHelper, getTriggers } = require('./postgresHelpers/triggerHelper');
 const queryConstants = require('./queryConstants');
+const { reorganizePrimaryKeys } = require('./postgresHelpers/reorganizePrimaryKeys');
 
 let currentSshTunnel = null;
 let _ = null;
@@ -304,10 +305,12 @@ module.exports = {
 			targetAttributes = setSubtypeFromSampledJsonValues(targetAttributes, documents);
 		}
 
+		const { attributes, entityLevel: updatedEntityLevel } = reorganizePrimaryKeys(targetAttributes, entityLevel);
+
 		return {
 			name: tableName,
-			entityLevel,
-			jsonSchema: getJsonSchema(targetAttributes),
+			entityLevel: updatedEntityLevel,
+			jsonSchema: getJsonSchema(attributes),
 			documents,
 			relationships,
 		};
