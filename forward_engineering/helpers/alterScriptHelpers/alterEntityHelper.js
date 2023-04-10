80fe4faec0710debc49df0c6d28f446011bd4414
unknown <unknown>
2023-04-10 17:21:45 +0300

    Fixed null constraint assignment for renamed columns

--- forward_engineering/helpers/alterScriptHelpers/alterEntityHelper.js
+++ forward_engineering/helpers/alterScriptHelpers/alterEntityHelper.js
@@ -185,11 +185,20 @@ const getModifyNonNullColumnsScripts = (_, ddlProvider) => (collection) => {
     const columnNamesToAddNotNullConstraint = _.difference(currentRequiredColumnNames, previousRequiredColumnNames);
     const columnNamesToRemoveNotNullConstraint = _.difference(previousRequiredColumnNames, currentRequiredColumnNames);
 
-    const addNotNullConstraintsScript = columnNamesToAddNotNullConstraint
-        .map(columnName => ddlProvider.setNotNullConstraint(fullTableName, wrapInQuotes(columnName)));
+    const addNotNullConstraintsScript = _.toPairs(collection.properties)
+        .filter(([name, jsonSchema]) => {
+            const oldName = jsonSchema.compMod.oldField.name;
+            const shouldRemoveForOldName = columnNamesToRemoveNotNullConstraint.includes(oldName);
+            const shouldAddForNewName = columnNamesToAddNotNullConstraint.includes(name);
+            return shouldAddForNewName && !shouldRemoveForOldName;
+        })
+        .map(([columnName]) => ddlProvider.setNotNullConstraint(fullTableName, wrapInQuotes(columnName)));
     const removeNotNullConstraint = _.toPairs(collection.properties)
         .filter(([name, jsonSchema]) => {
-            return columnNamesToRemoveNotNullConstraint.includes(jsonSchema.compMod.oldField.name)
+            const oldName = jsonSchema.compMod.oldField.name;
+            const shouldRemoveForOldName = columnNamesToRemoveNotNullConstraint.includes(oldName);
+            const shouldAddForNewName = columnNamesToAddNotNullConstraint.includes(name);
+            return shouldRemoveForOldName && !shouldAddForNewName;
         })
         .map(([name]) => ddlProvider.dropNotNullConstraint(fullTableName, wrapInQuotes(name)));
 
