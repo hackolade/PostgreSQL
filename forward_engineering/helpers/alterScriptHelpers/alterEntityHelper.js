c889407b812ffec5dbfcb1445ac56e518caaf902
unknown <unknown>
2023-04-21 11:13:20 +0300

    Improved lookup of updated string/numeric attributes which fixes the problem with custom naming conventions

--- forward_engineering/helpers/alterScriptHelpers/alterEntityHelper.js
+++ forward_engineering/helpers/alterScriptHelpers/alterEntityHelper.js
@@ -123,27 +123,21 @@ const getFullTableName = (_) => (collection) => {
     return getNamePrefixedWithSchemaName(tableName, schemaName);
 }
 
-const extractNewPropertyByName = (collection, fieldName) => {
-    return collection.role.compMod?.newProperties?.find(newProperty => newProperty.name === fieldName);
-}
-
-const hasLengthChanged = (collection, newFieldName, oldFieldName) => {
+const hasLengthChanged = (collection, oldFieldName, currentJsonSchema) => {
     const oldProperty = collection.role.properties[oldFieldName];
-    const newProperty = extractNewPropertyByName(collection, newFieldName);
 
     const previousLength = oldProperty?.length;
-    const newLength = newProperty?.length;
+    const newLength = currentJsonSchema?.length;
     return previousLength !== newLength;
 }
 
-const hasPrecisionOrScaleChanged = (collection, newFieldName, oldFieldName) => {
+const hasPrecisionOrScaleChanged = (collection, oldFieldName, currentJsonSchema) => {
     const oldProperty = collection.role.properties[oldFieldName];
-    const newProperty = extractNewPropertyByName(collection, newFieldName);
 
     const previousPrecision = oldProperty?.precision;
-    const newPrecision = newProperty?.precision;
+    const newPrecision = currentJsonSchema?.precision;
     const previousScale = oldProperty?.scale;
-    const newScale = newProperty?.scale;
+    const newScale = currentJsonSchema?.scale;
 
     return previousPrecision !== newPrecision || previousScale !== newScale;
 }
@@ -157,8 +151,8 @@ const getUpdateTypesScripts = (_, ddlProvider) => (collection) => {
             const hasTypeChanged = checkFieldPropertiesChanged(jsonSchema.compMod, ['type', 'mode']);
             if (!hasTypeChanged) {
                 const oldName = jsonSchema.compMod.oldField.name;
-                const isNewLength = hasLengthChanged(collection, name, oldName);
-                const isNewPrecisionOrScale = hasPrecisionOrScaleChanged(collection, name, oldName);
+                const isNewLength = hasLengthChanged(collection, oldName, jsonSchema);
+                const isNewPrecisionOrScale = hasPrecisionOrScaleChanged(collection, oldName, jsonSchema);
                 return isNewLength || isNewPrecisionOrScale;
             }
             return hasTypeChanged;
@@ -167,8 +161,7 @@ const getUpdateTypesScripts = (_, ddlProvider) => (collection) => {
             ([name, jsonSchema]) => {
                 const typeName = jsonSchema.compMod.newField.mode || jsonSchema.compMod.newField.type;
                 const columnName = wrapInQuotes(name);
-                const newProperty = extractNewPropertyByName(collection, name);
-                const typeConfig = _.pick(newProperty, ['length', 'precision', 'scale']);
+                const typeConfig = _.pick(jsonSchema, ['length', 'precision', 'scale']);
                 return ddlProvider.alterColumnType(fullTableName, columnName, typeName, typeConfig);
             }
         );
