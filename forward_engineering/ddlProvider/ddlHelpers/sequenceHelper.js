1345f30da603c525c99892601f652503d348f388
Serhii Filonenko <serhii.filonenko@binary-studio.com>
2024-02-22 16:54:04 +0200

    RE: fix missed arguments wrapping

--- forward_engineering/ddlProvider/ddlHelpers/sequenceHelper.js
+++ forward_engineering/ddlProvider/ddlHelpers/sequenceHelper.js
@@ -73,7 +73,7 @@ module.exports = ({
 		];
 
 		const options = optionConfigs
-			.map((config) => wrapOption(config.getOption(sequence, config)))
+			.map((config) => wrapOption(config.getOption({ sequence, config })))
 			.filter(Boolean)
 			.join('');
 
